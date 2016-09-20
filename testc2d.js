4c5500ff700773ddb6b3d89e74898adadc516c7d
Guy Bertental <guybartal@gmail.com>
2016-09-20 17:14:55 +0300

    test c2d

--- testc2d.js
+++ testc2d.js
@@ -8,29 +8,42 @@ var targetDevice = 'HomeIotGateway';
 
 var client = Client.fromConnectionString(connectionString);
 
-client.open(function (err) {
-  if (err) {
-    console.error('Could not connect: ' + err.message);
-  } else {
-    console.log('Client connected');
 
-    // Create a message and send it to the IoT Hub every second
-    setInterval(function () {
-      var data = JSON.stringify({ text : 'foo' });
-      var message = new Message(data);
-      console.log('Sending message: ' + message.getData());
-      client.send(targetDevice, message, printResultFor('send'));
-    }, 2000);
-  }
+client.open(function (err) {
+    if (err) {
+        console.error('Could not connect: ' + err.message);
+    } else {
+        console.log('Client connected');
+client.getFeedbackReceiver(receiveFeedback);
+
+        // Create a message and send it to the IoT Hub every second
+        setInterval(function () {
+            var data = JSON.stringify({ text: 'foo' });
+            var message = new Message(data);
+            message.ack = 'full';
+            message.messageId = 'TurnOnLightsInRoom';
+            console.log('Sending message: ' + message.getData());
+            client.send(targetDevice, message, printResultFor('send'));
+        }, 2000);
+    }
 });
 
 // Helper function to print results in the console
 function printResultFor(op) {
-  return function printResult(err, res) {
-    if (err) {
-      console.log(op + ' error: ' + err.toString());
-    } else {
-      console.log(op + ' status: ' + res.constructor.name);
-    }
-  };
+    return function printResult(err, res) {
+        if (err) {
+            console.log(op + ' error: ' + err.toString());
+        } else {
+            console.log(op + ' status: ' + res.constructor.name);
+        }
+    };
+}
+
+
+function receiveFeedback(err, receiver) {
+    receiver.on('message', function (msg) {
+        console.log('Feedback message:');
+        var feedbackData = msg.getData();
+        console.log(feedbackData.toString('utf-8'));
+    });
 }
